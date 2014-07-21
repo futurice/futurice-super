92c1cfe27f4c43b196d4f82913f0e08cb23800de
Oskar Ehnstrom <oskar.e@gmail.com>
2014-07-21 13:09:50 +0300

    Add scheduler to db updater and rename it

--- db_updater.js
+++ db_updater.js
@@ -4,6 +4,7 @@ var credentials = require('./salesforce-credentials.js'),
   jsforce = require('jsforce'),
   _ = require('underscore'),
   nano = require('nano')('http://localhost:5984'),
+  schedule = require('node-schedule'),
   dbName = 'futurice-super',
   database = nano.use(dbName),
   conn = new jsforce.Connection({}),
@@ -12,7 +13,8 @@ var credentials = require('./salesforce-credentials.js'),
   addViews,
   addOrUpdateDocument,
   addOpportunities,
-  removeDeletedOpportunities;
+  removeDeletedOpportunities,
+  updateSalesforceData;
 
 /*
  * Utilities.
@@ -142,61 +144,84 @@ removeDeletedOpportunities = function(opportunities){
 /*
  * Salesforce logic.
  */
-conn.login(credentials.user, credentials.passwordtoken, function(err, userInfo) {
-  if (err) { return console.error(err); }
-
-  console.log("Bearer " + conn.accessToken);
-
-  console.log(conn.instanceUrl);
-
-  console.log("User ID: " + userInfo.id);
-  console.log("Org ID: " + userInfo.organizationId);
-
-  var closed_date = new Date(new Date() - (3*30*24*60*60*1000)); // Months, Days, Hours...
-  var dateArray = [
-    closed_date.getFullYear(),
-    zeropad(closed_date.getUTCMonth()+1, 2),
-    zeropad(closed_date.getUTCDay(), 2)
-  ];
-  var date = dateArray.join('-');
-
-
-console.log("Querying Salesforce.");
-
-conn.query('SELECT Id, Name, Account.Name, Account.Id, '+
-  'Owner.Name, Owner.Id, Owner.Alias, Description, Amount, CloseDate, '+
-  'Probability, StageName, IsClosed, IsWon, '+
-  'Budgeted_Work_End_Date__c, Budgeted_Work_Start_Date__c, Average_Hour_Price__c, '+
-  'Type, Futu_Team__c, LastModifiedDate, CreatedDate '+
-  'FROM Opportunity WHERE CloseDate > ' + date,
-  function(err, res) {
+updateSalesforceData = function() {
+  conn.login(credentials.user, credentials.passwordtoken, function(err, userInfo) {
     if (err) { return console.error(err); }
 
-    // Information about objects here:
-    // http://www.salesforce.com/us/developer/docs/api/Content/sforce_api_objects_opportunity.htm
+    console.log("Bearer " + conn.accessToken);
 
-    // "Describe" in API:
-    // https://eu1.salesforce.com/services/data/v30.0/sobjects/Opportunity/describe
+    console.log(conn.instanceUrl);
 
-    console.log('Done: ' + res.done);
-    console.log("Fetched Opportunities from Salesforce.");
+    console.log("User ID: " + userInfo.id);
+    console.log("Org ID: " + userInfo.organizationId);
 
-    console.log("Checking for database.");
-    createDatabase(function(){
-      console.log("Database created successfully.");
-      addOpportunities(res.records);
-    }, function(err){
-          if (err.error === 'file_exists') {
-            console.log("Database exists.");
-            addOpportunities(res.records);
-            removeDeletedOpportunities(res.records);
-          } else {
-            console.log(err);
-          }
+    var closed_date = new Date(new Date() - (3*30*24*60*60*1000)); // Months, Days, Hours...
+    var dateArray = [
+      closed_date.getFullYear(),
+      zeropad(closed_date.getUTCMonth()+1, 2),
+      zeropad(closed_date.getUTCDay(), 2)
+    ];
+    var date = dateArray.join('-');
+
+
+  console.log("Querying Salesforce.");
+
+  conn.query('SELECT Id, Name, Account.Name, Account.Id, '+
+    'Owner.Name, Owner.Id, Owner.Alias, Description, Amount, CloseDate, '+
+    'Probability, StageName, IsClosed, IsWon, '+
+    'Budgeted_Work_End_Date__c, Budgeted_Work_Start_Date__c, Average_Hour_Price__c, '+
+    'Type, Futu_Team__c, LastModifiedDate, CreatedDate '+
+    'FROM Opportunity WHERE CloseDate > ' + date,
+    function(err, res) {
+      if (err) { return console.error(err); }
+
+      // Information about objects here:
+      // http://www.salesforce.com/us/developer/docs/api/Content/sforce_api_objects_opportunity.htm
+
+      // "Describe" in API:
+      // https://eu1.salesforce.com/services/data/v30.0/sobjects/Opportunity/describe
+
+      console.log('Done: ' + res.done);
+      console.log("Fetched Opportunities from Salesforce.");
+
+      console.log("Checking for database.");
+      createDatabase(function(){
+        console.log("Database created successfully.");
+        addOpportunities(res.records);
+      }, function(err){
+            if (err.error === 'file_exists') {
+              console.log("Database exists.");
+              addOpportunities(res.records);
+              removeDeletedOpportunities(res.records);
+            } else {
+              console.log(err);
+            }
+      });
     });
-
   });
+};
+
+/*
+ * Schedule updating the DB
+ */
+
+// Every 30minutes during work hours.
+var workHours = schedule.scheduleJob('*/30 8-18 * * 1-5', function(){
+  updateSalesforceData();
+});
+
+// Certain times during the evening/night on weekdays.
+var weekNights = schedule.scheduleJob('0 20,22,0,6 * * 1-5', function(){
+  updateSalesforceData();
+});
+
+// Weekends, every four hours.
+var weekends = schedule.scheduleJob('0 */4 * * 0,6', function(){
+  updateSalesforceData();
 });
 
 
-
+/*
+ * Run an update
+ */
+ updateSalesforceData();
